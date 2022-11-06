ece5f45e320145bbd6d3ee860d546de6ded65089
Ntyax <113288623+Ntyax@users.noreply.github.com>
2022-11-06 15:12:04 +0200

    add mechanical

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from pygame import*
+from random import randint
 
 mixer.init
 
-WIDTH = 1500
-HEIGHT = 800
+WIDTH = 1200
+HEIGHT = 690
 window = display.set_mode((WIDTH,HEIGHT))
 display.set_caption("runner")
 clock = time.Clock()
+groundlvl = 630
 
 class GameSpirite (sprite.Sprite):
     def __init__(self,image_name,x,y,width, height):
@@ -23,35 +25,97 @@ class GameSpirite (sprite.Sprite):
 
 class Player(GameSpirite):
     def __init__(self):
-        super().__init__("player.png",70,660,75,75)
-        self.speed_y = 0
-        self.jump_speed = 50
+        super().__init__("player.png",500,groundlvl - 75,75,75)
+        self.speed_y = 10
+        self.jump_speed = 30
         self.speed = 15
-        self.onground = False
+        self.onground = True
+        self.wh = 5
+        self.gravity = 1
+        self.jumpcount = 0
 
+    def jump (self):
+        self.rect.y -= self.jump_speed
+        self.onground = False
+        self.gravity = 0
+        self.speed_y = 5
+        
+ 
     def update(self):
         keys = key.get_pressed()
         if keys [K_LEFT] and self.rect.x > 0:
             self.rect.x -= self.speed
         if keys [K_RIGHT] and self.rect.x < WIDTH - self.width:
             self.rect.x += self.speed
-        if keys [K_SPACE] and self.rect.x > WIDTH - self.width:
-            self.rect.y -= self.jump_speed
-        #if self.speed.rect.x < 0
+        if keys [K_SPACE]:
+            self.jump()
+        #if keys [K_UP]:
+            #self.rect.y -= self.jump_speed
+        #if keys [K_DOWN]:
+            #self.rect.y += self.jump_speed
+        if self.wh == 0:
+            player.rect.y = 570
+        if not self.onground:
+            self.gravity += 1
+            self.rect.y += self.speed_y + self.gravity
+        if self.rect.y >= 570:
+            self.onground = True
+            self.gravity = 0
+            self.speed_y = 0
+        if self.onground == False:
+            start_num = 7
+            start_num - 1
+            if start_num == 0:
+                print('jfiwjf ')
 
-        self.rect.y += self.speed_y
+class Kakt (GameSpirite):
+    def __init__(self):
+        rand_h = randint(75,150)
+        rand_x = randint(0,WIDTH)
+        super().__init__("kakt.png",WIDTH+rand_x,groundlvl - rand_h , int(rand_h / 2) ,rand_h)
+       
+      
+    def update(self):
+        self.rect.x -= bg_speed
+        if self.rect.x <= -100:
+            self.kill()
+      
 
+fon1_x = 0
+fon2_x = WIDTH
+
+
+
+bg_speed = 7
 fon = transform.scale(image.load('fon.png'), (WIDTH, HEIGHT))
+fon2 = transform.scale(image.load('fon.png'), (WIDTH, HEIGHT))
 run = True
 FPS = 60
 player = Player()
+kakts = sprite.Group()
+
+
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
 
-        window.blit (fon, (0,0))
-        player.update()
-        player.draw()
-        display.update()
-        clock.tick(FPS)
+    if fon1_x < - WIDTH:
+        fon1_x = WIDTH
+
+    if fon2_x < - WIDTH:
+        fon2_x = WIDTH
+    rand_num = randint(0,100)
+    if rand_num == 70:
+        kakts.add(Kakt())
+
+    fon1_x -= bg_speed
+    fon2_x -= bg_speed
+    window.blit (fon, (fon1_x,0))
+    window.blit (fon2, (fon2_x,0))
+    player.update()
+    player.draw()
+    kakts.update()
+    kakts.draw(window)
+    display.update()
+    clock.tick(FPS)
