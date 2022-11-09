9e4bfd10243984bff2959bf2b0b977e5ce854e67
BanderaYura <yuradopilkolviv@gmail.com>
2022-11-09 20:53:43 +0200

    add text

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from pygame import*
 from random import randint
 
-mixer.init
+mixer.init()
+font.init()
 
 WIDTH = 1200
 HEIGHT = 690
 window = display.set_mode((WIDTH,HEIGHT))
 display.set_caption("runner")
 clock = time.Clock()
-groundlvl = 630
+
+groundlvl = 660
+
 
 class GameSpirite (sprite.Sprite):
     def __init__(self,image_name,x,y,width, height):
@@ -39,6 +42,8 @@ class Player(GameSpirite):
         self.onground = False
         self.gravity = 0
         self.speed_y = 5
+        
+
         
  
     def update(self):
@@ -73,12 +78,17 @@ class Kakt (GameSpirite):
         rand_h = randint(75,150)
         rand_x = randint(0,WIDTH)
         super().__init__("kakt.png",WIDTH+rand_x,groundlvl - rand_h , int(rand_h / 2) ,rand_h)
+        self.rect.bottom = groundlvl
        
-      
+  
     def update(self):
+        global points,count_text
         self.rect.x -= bg_speed
         if self.rect.x <= -100:
             self.kill()
+            points += 1
+            count_text = font1.render(str(points),True,(0,255,0))
+        
       
 
 fon1_x = 0
@@ -90,15 +100,21 @@ bg_speed = 7
 fon = transform.scale(image.load('fon.png'), (WIDTH, HEIGHT))
 fon2 = transform.scale(image.load('fon.png'), (WIDTH, HEIGHT))
 run = True
+game = True
 FPS = 60
 player = Player()
+points = 0
 kakts = sprite.Group()
 
-
+font1 = font.SysFont("Impact",50)
+result = font1.render('Ти програв',True,(0,255,0))
+count_text = font1.render('0',True,(0,255,0))
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+        if e.type == KEYDOWN:
+            player.jump()
 
     if fon1_x < - WIDTH:
         fon1_x = WIDTH
@@ -108,14 +124,25 @@ while run:
     rand_num = randint(0,100)
     if rand_num == 70:
         kakts.add(Kakt())
+    collide = sprite.spritecollideany(player,kakts)
+    if collide:
+        game = False
+    if game:
+        fon1_x -= bg_speed
+        fon2_x -= bg_speed
+        window.blit (fon, (fon1_x,0))
+        window.blit (fon2, (fon2_x,0))
+        player.update()
+        player.draw()
+        kakts.update()
+        kakts.draw(window)
+    else:
+        window.blit(result, (300,300))
 
-    fon1_x -= bg_speed
-    fon2_x -= bg_speed
-    window.blit (fon, (fon1_x,0))
-    window.blit (fon2, (fon2_x,0))
-    player.update()
-    player.draw()
-    kakts.update()
-    kakts.draw(window)
+    window.blit(count_text,(30,30))
+
+
+
+
     display.update()
     clock.tick(FPS)
